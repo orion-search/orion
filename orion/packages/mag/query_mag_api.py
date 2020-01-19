31a707cc07e8c17fa0265af011664a8871694e0e
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-01-19 11:31:46 +0000

    query mag with FoS

--- orion/packages/mag/query_mag_api.py
+++ orion/packages/mag/query_mag_api.py
@@ -1,6 +1,7 @@
 """
 This code is based on the work we have done at Nesta. You can find the original here: https://github.com/nestauk/nesta/blob/dev/nesta/packages/mag/query_mag_api.py
 """
+import logging
 import requests
 from collections import defaultdict
 from retrying import retry
@@ -127,3 +128,87 @@ def dedupe_entities(entities):
         deduped_mag_ids.add(sorted(title, key=title.get, reverse=True)[0])
 
     return deduped_mag_ids
+
+
+def query_fields_of_study(
+    subscription_key,
+    ids=None,
+    levels=None,
+    fields=["Id", "DFN", "FL", "FP.FId", "FC.FId"],
+    # id, display_name, level, parent_ids, children_ids
+    query_count=1000,
+    results_limit=None,
+):
+    """Queries the MAG for fields of study. Expect >650k results for all levels.
+
+    Args:
+        subscription_key (str): MAG api subscription key
+        ids: (:obj:`list` of `int`): field of study ids to query
+        levels (:obj:`list` of `int`): levels to extract. 0 is highest, 5 is lowest
+        fields (:obj:`list` of `str`): codes of fields to return, as per mag documentation
+        query_count (int): number of items to return from each query
+        results_limit (int): break and return as close to this number of results as the
+            offset and query_count allow (for testing)
+
+    Returns:
+        (:obj:`list` of `dict`): processed results from the api query
+    
+    """
+    if ids is not None and levels is None:
+        expr_args = (ids, "Id")
+    elif levels is not None and ids is None:
+        expr_args = (levels, "FL")
+    else:
+        raise TypeError("Field of study ids OR levels should be supplied")
+
+    field_mapping = {
+        "Id": "id",
+        "DFN": "name",
+        "FL": "level",
+        "FP": "parent_ids",
+        "FC": "child_ids",
+    }
+    fields_to_drop = ["logprob", "prob"]
+    fields_to_compact = ["parent_ids", "child_ids"]
+
+    for expr in build_expr(*expr_args):
+        count = 1000
+        offset = 0
+        while True:
+            fos_data = query_mag_api(
+                expr,
+                fields,
+                subscription_key=subscription_key,
+                query_count=count,
+                offset=offset,
+            )
+            if fos_data["entities"] == []:
+                logging.info("Empty entities returned, no more data")
+                break
+
+            # clean up and formatting
+            for row in fos_data["entities"]:
+                for f in fields_to_drop:
+                    del row[f]
+
+                for code, description in field_mapping.items():
+                    try:
+                        row[description] = row.pop(code)
+                    except KeyError:
+                        pass
+
+                for field in fields_to_compact:
+                    try:
+                        row[field] = [ids["FId"] for ids in row[field]]
+                    except KeyError:
+                        # no parents and/or children
+                        pass
+
+                logging.info(f"new fos: {row}")
+                yield row
+
+            offset += len(fos_data["entities"])
+            logging.info(offset)
+
+            if results_limit is not None and offset >= results_limit:
+                break
