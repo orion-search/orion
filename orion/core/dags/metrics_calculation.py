3b259b59a24f6765879f3be11064eeb09643dd27
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-01-30 15:49:02 +0000

    rca dag

---
+++ orion/core/dags/metrics_calculation.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+import orion
+from airflow.operators.dummy_operator import DummyOperator
+from orion.core.airflow_utils import misctools
+from orion.core.operators.calculate_metrics_task import RCAOperator
+
+default_args = {
+    "owner": "Kostas St",
+    "start_date": datetime(2019, 11, 22),
+    "depends_on_past": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=2),
+}
+
+DAG_ID = "mag_metrics"
+DB_CONFIG = misctools.get_config("orion_config.config", "postgresdb")["database_uri"]
+# rds_config = misctools.get_config("orion_config.config", "postgresdb")["rds"]
+
+
+with DAG(
+    dag_id=DAG_ID, default_args=default_args, schedule_interval=timedelta(days=365)
+) as dag:
+
+    dummy_task = DummyOperator(task_id="start")
+
+    rca = RCAOperator(task_id="rca_measurement", db_config=DB_CONFIG)
+
+    dummy_task >> rca
