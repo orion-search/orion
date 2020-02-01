b751ab9937acbb01d8b1a6fd756bc1506e368dff
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-02-01 15:17:34 +0000

    add all pipeline

--- orion/core/dags/mag_collection.py
+++ orion/core/dags/mag_collection.py
@@ -2,10 +2,17 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from orion.core.airflow_utils import misctools
-from orion.core.operators.mag_parse_task import MagParserOperator
+from orion.core.operators.mag_parse_task import MagParserOperator, FosFrequencyOperator
 from orion.core.operators.mag_geocode_task import GeocodingOperator
 from orion.core.operators.mag_collect_task import MagCollectionOperator, MagFosCollectionOperator
 import orion
+from orion.core.operators.infer_gender_task import (
+    NamesBatchesOperator,
+    GenderInferenceOperator,
+)
+from orion.core.operators.calculate_metrics_task import RCAOperator
+from orion.core.operators.text2vec_task import Text2VectorOperator
+from orion.core.operators.dim_reduction_task import DimReductionOperator
 
 default_args = {
     "owner": "Kostas St",
@@ -17,18 +24,40 @@ default_args = {
 
 DAG_ID = "mag_collection"
 DB_CONFIG = misctools.get_config("orion_config.config", "postgresdb")["orion"]
+DB_CONFIG_AWS = misctools.get_config("orion_config.config", "postgresdb")["rds"]
 MAG_API_KEY = misctools.get_config("orion_config.config", "mag")["mag_api_key"]
-
 mag_config = orion.config["data"]["mag"]
 
-# task 1
+# task 1: 
 MAG_OUTPUT_BUCKET = "mag-data-dump"
 query_values = mag_config["query_values"]
 entity_name = mag_config["entity_name"]
 metadata = mag_config["metadata"]
 
-# task 3
+# task 3: geocode places
 google_key = misctools.get_config("orion_config.config", "google")["google_key"]
+
+# task 6: batch names
+BATCH_SIZE = 20000
+S3_BUCKET = "names-batches"
+PREFIX = "batched_names"
+
+# task 7: infer gender
+parallel_tasks = 4
+auth_token = misctools.get_config("orion_config.config", "genderapi")["auth"]
+
+# task 8: text embeddings
+text_vectors_prefix = "doc_vectors"
+text_vectors_bucket = "document-vectors"
+
+# task 9: dimensionality reduction
+config = orion.config["umap"]
+# umap hyperparameters
+n_neighbors = config["n_neighbors"]
+n_components = config["n_components"]
+metric = config["metric"]
+min_dist = config["min_dist"]
+
 
 with DAG(
     dag_id=DAG_ID, default_args=default_args, schedule_interval=timedelta(days=365)
@@ -53,5 +82,47 @@ with DAG(
 
     collect_fos = MagFosCollectionOperator(task_id='collect_fos_metadata', db_config=DB_CONFIG, subscription_key=MAG_API_KEY)
 
-    dummy_task >> query_mag >> parse_mag >> geocode_places
-    parse_mag >> collect_fos
+    fos_frequency = FosFrequencyOperator(task_id='fos_frequency', db_config=DB_CONFIG)
+
+    batch_names = NamesBatchesOperator(
+        task_id="batch_names",
+        db_config=DB_CONFIG,
+        s3_bucket=S3_BUCKET,
+        prefix=PREFIX,
+        batch_size=BATCH_SIZE,
+    )
+
+    batch_task_gender = []
+    for parallel_task in range(parallel_tasks):
+        task_id = f"gender_inference_{parallel_task}"
+        batch_task_gender.append(
+            GenderInferenceOperator(
+                task_id=task_id,
+                db_config=DB_CONFIG_AWS,
+                s3_bucket=S3_BUCKET,
+                prefix=f"{PREFIX}_{parallel_task}",
+                auth_token=auth_token,
+            )
+        )
+
+    rca = RCAOperator(task_id="rca_measurement", db_config=DB_CONFIG)
+
+    text2vector = Text2VectorOperator(
+        task_id="text2vector", db_config=DB_CONFIG, bucket=text_vectors_bucket, prefix=text_vectors_prefix,
+    )
+
+    dim_reduction = DimReductionOperator(
+        task_id="dim_reduction",
+        db_config=DB_CONFIG,
+        bucket=text_vectors_bucket,
+        prefix=text_vectors_prefix,
+        n_neighbors=n_neighbors,
+        min_dist=min_dist,
+        n_components=n_components,
+        metric=metric,
+    )
+
+    dummy_task >> query_mag >> parse_mag >> geocode_places >> rca
+    parse_mag >> collect_fos >>  fos_frequency
+    parse_mag >> batch_names >> batch_task_gender
+    parse_mag >> text2vector >> dim_reduction
