3463b98da9a247ba111a85a5df691d6ef2f6684e
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-02-01 15:18:23 +0000

    process inverted abstracts

--- orion/core/operators/text2vec_task.py
+++ orion/core/operators/text2vec_task.py
@@ -10,18 +10,13 @@ from sqlalchemy.orm import sessionmaker
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 from orion.packages.nlp.text2vec import Text2Vector
-from orion.core.orms.bioarxiv_orm import Article
 from orion.core.orms.mag_orm import Paper, DocVector
 from orion.packages.utils.s3_utils import store_on_s3
+from orion.packages.utils.utils import inverted2abstract
 
 
 class Text2VectorOperator(BaseOperator):
-    """Transforms text to document embeddings.
-    
-    TODO: 
-    * Collect and use MAG's `inverted abstract` field to make the package more generalisable.
-    
-    """
+    """Transforms text to document embeddings."""
 
     # template_fields = ['']
     @apply_defaults
@@ -39,23 +34,24 @@ class Text2VectorOperator(BaseOperator):
 
         # Get the abstracts of bioRxiv papers.
         papers = (
-            s.query(Article.abstract, Paper.id, Paper.doi)
-            .join(Paper, Paper.doi == Article.doi)
+            s.query(Paper.inverted_abstract, Paper.id, Paper.doi)
             .filter(
                 and_(
                     ~exists().where(Paper.id == DocVector.id),
-                    Article.doi.isnot(None),
-                    Article.abstract.isnot(None),
+                    Paper.doi.isnot(None),
+                    Paper.inverted_abstract != 'NaN',
                 )
             )
         )
         logging.info(f"Number of documents to be vectorised: {papers.count()}")
-        papers = papers[:1000]
+        papers = papers[:10]
+        # TRANSFORM INVERTED_ABSTRACT TO ABSTRACT
         logging.info(f"Number of documents to be vectorised: {len(papers)}")
         tv = Text2Vector()
         vectors = []
-        for i, (abstract, id_, doi) in enumerate(papers):
+        for i, (inverted_abstract, id_, doi) in enumerate(papers):
             logging.info(f'{i}: {doi}')
+            abstract = inverted2abstract(inverted_abstract)
             vec = tv.average_vectors(tv.feature_extraction(tv.encode_text(abstract)))
             vectors.append([doi, vec, id_])
         logging.info("Embedding documents - Done!")
