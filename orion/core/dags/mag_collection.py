b5ae61858f63289dcaa3f22b3ba5149332694b96
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-02-01 13:34:38 +0000

    add conference parser test

--- orion/core/dags/mag_collection.py
+++ orion/core/dags/mag_collection.py
@@ -3,11 +3,8 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from orion.core.airflow_utils import misctools
 from orion.core.operators.mag_parse_task import MagParserOperator
-
-# from orion.core.operators.mag_process_titles_task import ProcessTitlesOperator
-# from orion.core.operators.mag_collect_task import MagCollectionOperator, MagFosCollectionOperator
-# from orion.core.operators.mag_geocode_task import GeocodingOperator
-from orion.core.operators.mag_collect_task_v2 import MagCollectionOperator
+from orion.core.operators.mag_geocode_task import GeocodingOperator
+from orion.core.operators.mag_collect_task import MagCollectionOperator, MagFosCollectionOperator
 import orion
 
 default_args = {
@@ -18,25 +15,20 @@ default_args = {
     "retry_delay": timedelta(minutes=2),
 }
 
-DAG_ID = "mag_collection_v2"
+DAG_ID = "mag_collection"
 DB_CONFIG = misctools.get_config("orion_config.config", "postgresdb")["orion"]
 MAG_API_KEY = misctools.get_config("orion_config.config", "mag")["mag_api_key"]
 
 mag_config = orion.config["data"]["mag"]
 
-# # task 1
-# BATCH_SIZE = 100
-# S3_BUCKET = "processed-mag-expressions-batches"
-# PREFIX = "processed-titles"
-
-# task 2
+# task 1
 MAG_OUTPUT_BUCKET = "mag-data-dump"
 query_values = mag_config["query_values"]
 entity_name = mag_config["entity_name"]
 metadata = mag_config["metadata"]
 
 # task 3
-# google_key = misctools.get_config("orion_config.config", "google")["google_key"]
+google_key = misctools.get_config("orion_config.config", "google")["google_key"]
 
 with DAG(
     dag_id=DAG_ID, default_args=default_args, schedule_interval=timedelta(days=365)
@@ -57,10 +49,9 @@ with DAG(
         task_id="parse_mag", s3_bucket=MAG_OUTPUT_BUCKET, db_config=DB_CONFIG
     )
 
-    # geocode_places = GeocodingOperator(task_id='geocode_places', db_config=DB_CONFIG, subscription_key=google_key)
+    geocode_places = GeocodingOperator(task_id='geocode_places', db_config=DB_CONFIG, subscription_key=google_key)
 
-    # collect_fos = MagFosCollectionOperator(task_id='collect_fos_metadata', db_config=DB_CONFIG, subscription_key=MAG_API_KEY)
+    collect_fos = MagFosCollectionOperator(task_id='collect_fos_metadata', db_config=DB_CONFIG, subscription_key=MAG_API_KEY)
 
-    dummy_task >> query_mag >> parse_mag
-    # >> geocode_places
-    # parse_mag >> collect_fos
+    dummy_task >> query_mag >> parse_mag >> geocode_places
+    parse_mag >> collect_fos
