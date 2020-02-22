4df58a54433a48bfa43c7b924fe2288bb330a535
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-02-22 17:15:51 +0000

    update country collaboration task

--- orion/core/operators/draw_collaboration_graph_task.py
+++ orion/core/operators/draw_collaboration_graph_task.py
@@ -9,10 +9,10 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 from orion.packages.utils.utils import cooccurrence_graph
 from orion.core.orms.mag_orm import (
-    PaperAuthor,
     AuthorAffiliation,
     AffiliationLocation,
     CountryCollaboration,
+    Paper,
 )
 
 
@@ -20,9 +20,10 @@ class CountryCollaborationOperator(BaseOperator):
     """Create a cooccurrence graph of country-level collaboration."""
 
     @apply_defaults
-    def __init__(self, db_config, *args, **kwargs):
+    def __init__(self, db_config, year, *args, **kwargs):
         super().__init__(**kwargs)
         self.db_config = db_config
+        self.year = year
 
     def execute(self, context):
         # Connect to postgresql db
@@ -33,38 +34,37 @@ class CountryCollaborationOperator(BaseOperator):
         # Load all the tables needed for the collaboration graph.
         aff_location = pd.read_sql(s.query(AffiliationLocation).statement, s.bind)
         author_aff = pd.read_sql(s.query(AuthorAffiliation).statement, s.bind)
-        paper_author = pd.read_sql(s.query(PaperAuthor).statement, s.bind)
+        papers = pd.read_sql(s.query(Paper).statement, s.bind)
 
         # Merge tables
         df = (
             aff_location[["affiliation_id", "country"]]
             .merge(author_aff, left_on="affiliation_id", right_on="affiliation_id")
-            .merge(
-                paper_author[["paper_id", "author_id"]],
-                left_on="author_id",
-                right_on="author_id",
-            )
+            .merge(papers[["id", "year"]], left_on="paper_id", right_on="id")
         )
 
         # Group countries by paper, remove duplicates and missing entries.
-        grouped_df = (
-            df[df.country != ""][["country", "paper_id"]]
-            .dropna()
-            .groupby("paper_id")["country"]
-            .apply(set)
-        )
-        logging.info(f"Grouped DF shape: {grouped_df.shape}")
-
-        # Create the cooccurrence graph
-        graph = cooccurrence_graph(grouped_df)
-
-        # Commit pairs in DB
-        for k, v in graph.items():
-            s.add(
-                CountryCollaboration(
-                    **{"country_a": k[0], "country_b": k[1], "weight": v}
-                )
+        for year in [year for year in sorted(df.year.unique()) if year > self.year]:
+            logging.info(f"Collaboration network for year: {year}")
+            grouped_df = (
+                df[(df.country != "") & (df.year == year)][["country", "paper_id"]]
+                .dropna()
+                .groupby("paper_id")["country"]
+                .apply(set)
             )
-            s.commit()
+            logging.info(f"Grouped DF shape: {grouped_df.shape}")
+            graph = cooccurrence_graph(grouped_df)
+            for k, v in graph.items():
+                s.add(
+                    CountryCollaboration(
+                        **{
+                            "country_a": k[0],
+                            "country_b": k[1],
+                            "weight": v,
+                            "year": year,
+                        }
+                    )
+                )
+                s.commit()
 
         logging.info("Done :)")
