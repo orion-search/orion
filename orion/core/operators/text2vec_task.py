aa69cd4f11c10d87b4cd71b5ed3355cfd6a9b8af
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-03-26 09:27:02 +0000

    adding pytorch and tensorflow in the dependencies

--- orion/core/operators/text2vec_task.py
+++ orion/core/operators/text2vec_task.py
@@ -23,8 +23,7 @@ from sqlalchemy.sql import exists
 from sqlalchemy.orm import sessionmaker
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
-# from orion.packages.nlp.text2vec import Text2Vector
-# , use_vectors
+from orion.packages.nlp.text2vec import Text2Vector, use_vectors
 from orion.core.orms.mag_orm import Paper, DocVector
 from orion.packages.utils.s3_utils import store_on_s3
 from orion.packages.utils.utils import inverted2abstract
@@ -37,45 +36,45 @@ seed = orion.config["seed"]
 max_features = orion.config["tfidf"]["max_features"]
 
 
-# class Text2VectorOperator(BaseOperator):
-#     """Transforms text to document embeddings."""
+class Text2VectorOperator(BaseOperator):
+    """Transforms text to document embeddings."""
 
-#     # template_fields = ['']
-#     @apply_defaults
-#     def __init__(self, db_config, bucket, prefix, *args, **kwargs):
-#         super().__init__(**kwargs)
-#         self.db_config = db_config
-#         self.bucket = bucket
-#         self.prefix = prefix
+    # template_fields = ['']
+    @apply_defaults
+    def __init__(self, db_config, bucket, prefix, *args, **kwargs):
+        super().__init__(**kwargs)
+        self.db_config = db_config
+        self.bucket = bucket
+        self.prefix = prefix
 
-#     def execute(self, context):
-#         # Connect to postgresql
-#         engine = create_engine(self.db_config)
-#         Session = sessionmaker(bind=engine)
-#         s = Session()
+    def execute(self, context):
+        # Connect to postgresql
+        engine = create_engine(self.db_config)
+        Session = sessionmaker(bind=engine)
+        s = Session()
 
-#         # Get the abstracts of bioRxiv papers.
-#         papers = s.query(Paper.inverted_abstract, Paper.id, Paper.doi).filter(
-#             and_(
-#                 ~exists().where(Paper.id == DocVector.id),
-#                 Paper.doi.isnot(None),
-#                 Paper.inverted_abstract != "NaN",
-#             )
-#         )
-#         logging.info(f"Number of documents to be vectorised: {papers.count()}")
+        # Get the abstracts of bioRxiv papers.
+        papers = s.query(Paper.inverted_abstract, Paper.id, Paper.doi).filter(
+            and_(
+                ~exists().where(Paper.id == DocVector.id),
+                Paper.doi.isnot(None),
+                Paper.inverted_abstract != "NaN",
+            )
+        )
+        logging.info(f"Number of documents to be vectorised: {papers.count()}")
 
-#         # Convert text to vectors
-#         tv = Text2Vector()
-#         vectors = []
-#         for i, (inverted_abstract, id_, doi) in enumerate(papers):
-#             logging.info(f"{i}: {doi}")
-#             abstract = inverted2abstract(inverted_abstract)
-#             vec = tv.average_vectors(tv.feature_extraction(tv.encode_text(abstract)))
-#             vectors.append([doi, vec, id_])
-#         logging.info("Embedding documents - Done!")
+        # Convert text to vectors
+        tv = Text2Vector()
+        vectors = []
+        for i, (inverted_abstract, id_, doi) in enumerate(papers):
+            logging.info(f"{i}: {doi}")
+            abstract = inverted2abstract(inverted_abstract)
+            vec = tv.average_vectors(tv.feature_extraction(tv.encode_text(abstract)))
+            vectors.append([doi, vec, id_])
+        logging.info("Embedding documents - Done!")
 
-#         store_on_s3(vectors, self.bucket, self.prefix)
-#         logging.info("Stored to S3!")
+        store_on_s3(vectors, self.bucket, self.prefix)
+        logging.info("Stored to S3!")
 
 
 class Text2TfidfOperator(BaseOperator):
@@ -130,41 +129,41 @@ class Text2TfidfOperator(BaseOperator):
         logging.info("Stored to S3!")
 
 
-# class Text2USEOperator(BaseOperator):
-#     """Transform text to vectors with the Universal Sentence Encoder model."""
+class Text2USEOperator(BaseOperator):
+    """Transform text to vectors with the Universal Sentence Encoder model."""
 
-#     @apply_defaults
-#     def __init__(self, db_config, bucket, prefix, *args, **kwargs):
-#         super().__init__(**kwargs)
-#         self.db_config = db_config
-#         self.bucket = bucket
-#         self.prefix = prefix
+    @apply_defaults
+    def __init__(self, db_config, bucket, prefix, *args, **kwargs):
+        super().__init__(**kwargs)
+        self.db_config = db_config
+        self.bucket = bucket
+        self.prefix = prefix
 
-#     def execute(self, context):
-#         # Connect to postgresql
-#         engine = create_engine(self.db_config)
-#         Session = sessionmaker(bind=engine)
-#         s = Session()
+    def execute(self, context):
+        # Connect to postgresql
+        engine = create_engine(self.db_config)
+        Session = sessionmaker(bind=engine)
+        s = Session()
 
-#         # Get the abstracts of bioRxiv papers.
-#         papers = s.query(Paper.inverted_abstract, Paper.id, Paper.doi).filter(
-#             and_(
-#                 ~exists().where(Paper.id == DocVector.id),
-#                 Paper.doi.isnot(None),
-#                 Paper.inverted_abstract != "NaN",
-#             )
-#         )
-#         logging.info(f"Number of documents to be vectorised: {papers.count()}")
+        # Get the abstracts of bioRxiv papers.
+        papers = s.query(Paper.inverted_abstract, Paper.id, Paper.doi).filter(
+            and_(
+                ~exists().where(Paper.id == DocVector.id),
+                Paper.doi.isnot(None),
+                Paper.inverted_abstract != "NaN",
+            )
+        )
+        logging.info(f"Number of documents to be vectorised: {papers.count()}")
 
-#         # Reconstruct abstracts
-#         inverted_abstracts, ids, doi = zip(*papers)
-#         abstracts = [inverted2abstract(abstract) for abstract in inverted_abstracts]
-#         assert len(abstracts) == len(ids)
-#         logging.info(f"Number of abstracts to vectorise: {len(abstracts)}")
+        # Reconstruct abstracts
+        inverted_abstracts, ids, doi = zip(*papers)
+        abstracts = [inverted2abstract(abstract) for abstract in inverted_abstracts]
+        assert len(abstracts) == len(ids)
+        logging.info(f"Number of abstracts to vectorise: {len(abstracts)}")
 
-#         # Vectorisation with USE
-#         vectors = use_vectors(abstracts)
-#         logging.info("Finished vectorisation :)")
-#         vectors = [[doi, vec, id_] for doi, vec, id_ in zip(doi, vectors, ids)]
-#         store_on_s3(vectors, self.bucket, "use_vectors")
-#         logging.info("Stored to S3!")
+        # Vectorisation with USE
+        vectors = use_vectors(abstracts)
+        logging.info("Finished vectorisation :)")
+        vectors = [[doi, vec, id_] for doi, vec, id_ in zip(doi, vectors, ids)]
+        store_on_s3(vectors, self.bucket, "use_vectors")
+        logging.info("Stored to S3!")
